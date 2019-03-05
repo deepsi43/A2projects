96f205922dca4484805d419c5ed59ca27e4eff0c
unknown <deepsim543@gmail.com>
2019-03-05 23:56:47 +0530

    EASportswebsite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatCardModule} from '@angular/material/card';
 import { AppComponent } from './app.component';
 
 @NgModule({
